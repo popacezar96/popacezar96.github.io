af6118e56c956d58a3e5823cd3eeb07b832e598f
Popa Cezar-Constantin <47753370+popacezar96@users.noreply.github.com>
2021-08-09 15:41:59 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@
 		$body = $('body'),
 		$header = $('#header'),
 		$banner = $('#banner');
+		$navShow = $('#nav-show');
 
 	// Breakpoints.
 		breakpoints({
@@ -33,11 +34,13 @@
 
 			$window.on('resize', function() { $window.trigger('scroll'); });
 
-			$banner.scrollex({
+			$navShow.scrollex({
 				bottom:		$header.outerHeight(),
 				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
+				enter:		function() { $header.addClass('alt');
+					$nav.removeClass('alternative');},
+				leave:		function() { $header.removeClass('alt');
+			$nav.addClass('alternative');}
 			});
 
 		}
@@ -142,4 +145,205 @@
 
 			});
 
+
+			// Nav.
+				var $nav = $('#nav');
+
+				if ($nav.length > 0) {
+
+					// Links.
+						var $header_a = $header.find('a:not(.not-this-one)');
+
+						$header_a
+							.scrolly({
+								speed: 1000,
+								offset: function() { return $nav.height(); }
+							})
+							.on('click', function() {
+
+								var $this = $(this);
+
+								// External link? Bail.
+									if ($this.attr('href').charAt(0) != '#')
+										return;
+
+								// Deactivate all links.
+									$header_a
+										.removeClass('active')
+										.removeClass('active-locked');
+
+								// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+									$this
+										.addClass('active')
+										.addClass('active-locked');
+
+							})
+							.each(function() {
+
+								var	$this = $(this),
+									id = $this.attr('href'),
+									$section = $(id);
+
+								// No section for this link? Bail.
+									if ($section.length < 1)
+										return;
+
+								// Scrollex.
+									$section.scrollex({
+										mode: 'middle',
+										initialize: function() {
+
+											// Deactivate section.
+												if (browser.canUse('transition'))
+													$section.addClass('inactive');
+
+										},
+										enter: function() {
+
+											// Activate section.
+												$section.removeClass('inactive');
+
+											// No locked links? Deactivate all links and activate this section's one.
+												if ($header_a.filter('.active-locked').length == 0) {
+
+													$header_a.removeClass('active');
+													$this.addClass('active');
+
+												}
+
+											// Otherwise, if this section's link is the one that's locked, unlock it.
+												else if ($this.hasClass('active-locked'))
+													$this.removeClass('active-locked');
+
+										}
+									});
+
+							});
+
+				}
+
+			// Ce e mai sus, dar pt meniul din mijlocul ecranului.
+
+			if ($nav.length > 0) {
+
+				// Links.
+					var $menu_a = $menu.find('a');
+
+					$menu_a
+						.scrolly({
+							speed: 1000,
+							offset: function() { return $nav.height(); }
+						})
+						.on('click', function() {
+
+							var $this = $(this);
+
+							// External link? Bail.
+								if ($this.attr('href').charAt(0) != '#')
+									return;
+
+							// Deactivate all links.
+								$menu_a
+									.removeClass('active')
+									.removeClass('active-locked');
+
+							// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+								$this
+									.addClass('active')
+									.addClass('active-locked');
+
+						})
+						.each(function() {
+
+							var	$this = $(this),
+								id = $this.attr('href'),
+								$section = $(id);
+
+							// No section for this link? Bail.
+								if ($section.length < 1)
+									return;
+
+							// Scrollex.
+								$section.scrollex({
+									mode: 'middle',
+									initialize: function() {
+
+										// Deactivate section.
+											if (browser.canUse('transition'))
+												$section.addClass('inactive');
+
+									},
+									enter: function() {
+
+										// Activate section.
+											$section.removeClass('inactive');
+
+										// No locked links? Deactivate all links and activate this section's one.
+											if ($menu_a.filter('.active-locked').length == 0) {
+
+												$menu_a.removeClass('active');
+												$this.addClass('active');
+
+											}
+
+										// Otherwise, if this section's link is the one that's locked, unlock it.
+											else if ($this.hasClass('active-locked'))
+												$this.removeClass('active-locked');
+
+									}
+								});
+						});
+			}
+
+			// Scrolly.
+				$('.scrolly').scrolly({
+					speed: 1000
+				});
+
 })(jQuery);
+
+
+// AJAX code added to the form from Formspree.com to modify default behaviour on submit
+
+var form = document.getElementById("my-form");
+
+	 async function handleSubmit(event) {
+		 event.preventDefault();
+		 var status = document.getElementById("form-buttons");
+		 var data = new FormData(event.target);
+		 fetch(event.target.action, {
+			 method: form.method,
+			 body: data,
+			 headers: {
+					 'Accept': 'application/json'
+			 }
+		 }).then(response => {
+
+			 var listItem = document.createElement("li");
+			 listItem.innerHTML = "<strong>Success!</strong> I'll get back to you!"
+			 listItem.setAttribute("class", "success");
+			 status.appendChild(listItem);
+
+			 form.reset();
+
+			 setTimeout(function(){
+			 	status.removeChild(status.lastElementChild);
+			}, 4000);
+
+
+		 }).catch(error => {
+
+			 var listItem = document.createElement("li");
+			 listItem.innerHTML = "<strong>Error!</strong> Contact me elswhere!"
+			 listItem.setAttribute("class", "error");
+			 status.appendChild(listItem);
+
+	 	 	form.reset();
+
+			setTimeout(function(){
+			 status.removeChild(status.lastElementChild);
+		 }, 4000);
+
+		 });
+	 }
+	 form.addEventListener("submit", handleSubmit)
